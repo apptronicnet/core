9f08d84a161f47da2e6c7e4efa169656c5fcc1bb
amakovskiy <andrey.makovskiy@outlook.com>
2019-09-16 12:20:16 +0300

    added DistinctUntilChangedEntity

---
+++ core_library_common/src/commonMain/kotlin/net/apptronic/core/component/entity/entities/DistinctUntilChangedEntity.kt
@@ -0,0 +1,44 @@
+package net.apptronic.core.component.entity.entities
+
+import net.apptronic.core.base.observable.Observer
+import net.apptronic.core.base.observable.subject.ValueHolder
+import net.apptronic.core.component.context.Context
+import net.apptronic.core.component.entity.Entity
+import net.apptronic.core.component.entity.EntitySubscription
+
+fun <T> Entity<T>.distinctUntilChanged(): Entity<T> {
+    return DistinctUntilChangedEntity(this)
+}
+
+class DistinctUntilChangedEntity<T>(
+        private val source: Entity<T>
+) : Entity<T> {
+
+    override fun getContext(): Context {
+        return source.getContext()
+    }
+
+    override fun subscribe(context: Context, observer: Observer<T>): EntitySubscription {
+        return source.subscribe(
+                context,
+                DistinctUntilChangedObserverWrapper(observer)
+        )
+    }
+
+}
+
+private class DistinctUntilChangedObserverWrapper<T>(
+        val target: Observer<T>
+) : Observer<T> {
+
+    private var lastValue: ValueHolder<T>? = null
+
+    override fun notify(value: T) {
+        val last = lastValue
+        if (last == null || last.value != value) {
+            lastValue = ValueHolder(value)
+            target.notify(value)
+        }
+    }
+
+}
