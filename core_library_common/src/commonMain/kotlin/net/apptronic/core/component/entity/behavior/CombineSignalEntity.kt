70fbf62d9bb3e229e37050dc613962c3870c0a5d
Andrii Makovskyi <andrey.makovskiy@outlook.com>
2020-07-20 11:12:38 +0300

    CombineSignalEntityTest

--- core_library_common/src/commonMain/kotlin/net/apptronic/core/component/entity/behavior/CombineSignalEntity.kt
+++ core_library_common/src/commonMain/kotlin/net/apptronic/core/component/entity/behavior/CombineSignalEntity.kt
@@ -4,10 +4,13 @@ import net.apptronic.core.base.observable.subject.PublishSubject
 import net.apptronic.core.component.context.Context
 import net.apptronic.core.component.entity.Entity
 import net.apptronic.core.component.entity.base.SubjectEntity
+import net.apptronic.core.component.entity.entities.asEvent
 import net.apptronic.core.component.entity.subscribe
+import net.apptronic.core.component.entity.switchContext
 
 /**
- * This creates new Entity which emits Unit signal each time when any of [sources] emits any item.
+ * This creates new Entity which emits Unit signal each time when any of [sources] emits any item. Ignores values
+ * of currently set items in properties etc.
  */
 fun Context.combineAsSignals(vararg sources: Entity<*>): Entity<Unit> {
     return CombineSignalEntity(this, sources)
@@ -21,11 +24,16 @@ private class CombineSignalEntity(
     override val subject = PublishSubject<Unit>()
 
     init {
+        var started = false
+        // this is for ignoring any currently set items
         sources.forEach {
-            it.subscribe(context) {
-                subject.update(Unit)
+            it.switchContext(context).asEvent().subscribe {
+                if (started) {
+                    subject.update(Unit)
+                }
             }
         }
+        started = true
     }
 
 }
