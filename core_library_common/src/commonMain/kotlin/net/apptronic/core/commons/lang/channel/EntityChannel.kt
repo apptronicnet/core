d841df8d45755f6a0ebcd8c926f5afb362001c88
Andrii Makovskyi <andrey.makovskiy@outlook.com>
2020-11-27 14:39:27 +0200

    Tests for EntityChannel.kt

--- core_library_common/src/commonMain/kotlin/net/apptronic/core/commons/lang/channel/EntityChannel.kt
+++ core_library_common/src/commonMain/kotlin/net/apptronic/core/commons/lang/channel/EntityChannel.kt
@@ -1,12 +1,17 @@
 package net.apptronic.core.commons.lang.channel
 
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
 import net.apptronic.core.UnderDevelopment
 import net.apptronic.core.entity.Entity
 
 @UnderDevelopment
-fun <T> Entity<T>.asChannel(): Channel<T> {
-    val channel = Channel<T>()
+fun <T> Entity<T>.asChannel(
+        capacity: Int = Channel.RENDEZVOUS,
+        onBufferOverflow: BufferOverflow = BufferOverflow.SUSPEND,
+        onUndeliveredElement: ((T) -> Unit)? = null
+): Channel<T> {
+    val channel = Channel<T>(capacity, onBufferOverflow, onUndeliveredElement)
     subscribeSuspend {
         channel.send(it)
     }
--- core_library_common/src/jvmTest/kotlin/net/apptronic/core/commons/lang/channel/ChannelTest.kt
+++ core_library_common/src/jvmTest/kotlin/net/apptronic/core/commons/lang/channel/ChannelTest.kt
@@ -1,13 +1,27 @@
 package net.apptronic.core.commons.lang.channel
 
+import net.apptronic.core.BaseContextTest
+import net.apptronic.core.entity.commons.value
+import net.apptronic.core.record
 import org.junit.Test
-import kotlin.test.fail
 
-class ChannelTest {
+class ChannelTest : BaseContextTest() {
+
+    val source = value<String>()
+    val channel = source.asChannel()
+    val result = channel.asEntity(context)
+    val data = result.record()
 
     @Test
     fun notWrittenTest() {
-        fail("Test is not written yet")
+        source.set("1")
+        data.assertItems("1")
+
+        source.set("2")
+        data.assertItems("1", "2")
+
+        source.set("35")
+        data.assertItems("1", "2", "35")
     }
 
 }
