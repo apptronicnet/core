0ac30b2ce089a04bafc6896976efba1ef847a5ca
Andrii Makovskyi <andrey.makovskiy@outlook.com>
2020-12-24 18:16:04 +0200

    DataProviderReloadFromClientTest

--- core_library_common/src/jvmTest/kotlin/net/apptronic/core/commons/dataprovider/DataProviderReloadFromClientTest.kt
+++ core_library_common/src/jvmTest/kotlin/net/apptronic/core/commons/dataprovider/DataProviderReloadFromClientTest.kt
@@ -1,13 +1,63 @@
 package net.apptronic.core.commons.dataprovider
 
+import kotlinx.coroutines.runBlocking
+import net.apptronic.core.BaseContextTest
+import net.apptronic.core.commons.dataprovider.cache.SimpleDataProviderCache
+import net.apptronic.core.context.Context
+import net.apptronic.core.context.dependencyModule
+import net.apptronic.core.entity.commons.value
 import org.junit.Test
-import kotlin.test.fail
+import kotlin.test.assertEquals
 
-class DataProviderReloadFromClientTest {
+class DataProviderReloadFromClientTest : BaseContextTest() {
+
+    private val Descriptor = dataProviderDescriptor<Int, Int>()
+
+    private val cache = SimpleDataProviderCache<Int, Int>(100, { 1 })
+
+    private class SerialDataProvider(context: Context, key: Int) : DataProvider<Int, Int>(context, key) {
+
+        var value = 1
+
+        override val dataProviderEntity = value(value++)
+
+        override suspend fun loadData(): Int {
+            return value++
+        }
+
+    }
+
+    init {
+        context.dependencyModule {
+            dataProviderCache(Descriptor) {
+                cache
+            }
+            sharedDataProvider(Descriptor) { key ->
+                SerialDataProvider(scopedContext(), key)
+            }
+        }
+    }
 
     @Test
-    fun notWrittenYet() {
-        fail("Test is not written yet")
+    fun verifyLoadFromClientAndSaveToCache() {
+        runBlocking {
+            val data1 = injectData(Descriptor, 1)
+            val data2 = injectData(Descriptor, 2)
+
+            assertEquals(1, data1.get())
+            assertEquals(1, data2.get())
+
+            assertEquals(1, cache[1]!!.value)
+            assertEquals(1, cache[2]!!.value)
+
+            assertEquals(2, data1.reload())
+            assertEquals(2, data1.get())
+            assertEquals(2, cache[1]!!.value)
+
+            data1.postReload()
+            assertEquals(3, data1.get())
+            assertEquals(3, cache[1]!!.value)
+        }
     }
 
 }
