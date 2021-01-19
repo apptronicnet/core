485c2e9adca3ac829c77d31c71631d86cab0578a
Andrii Makovskyi <andrey.makovskiy@outlook.com>
2021-01-19 12:59:21 +0200

    EmptyEntity factory method

--- core_library_common/src/commonMain/kotlin/net/apptronic/core/entity/commons/EmptyEntity.kt
+++ core_library_common/src/commonMain/kotlin/net/apptronic/core/entity/commons/EmptyEntity.kt
@@ -2,16 +2,21 @@ package net.apptronic.core.entity.commons
 
 import net.apptronic.core.base.subject.PublishSubject
 import net.apptronic.core.context.Context
+import net.apptronic.core.context.Contextual
 import net.apptronic.core.entity.Entity
 import net.apptronic.core.entity.base.ObservableSubjectEntity
 
-/**
- * Simple [Entity] which never emits anything
- */
-class EmptyEntity(
-        override val context: Context
-) : ObservableSubjectEntity<Unit>() {
+fun <T> Contextual.emptyEntity(): Entity<T> {
+    return EmptyEntity(context)
+}
 
-    override val subject = PublishSubject<Unit>()
+/**
+ * Simple [Entity] which never emits anything.
+ */
+class EmptyEntity<T>(
+    override val context: Context
+) : ObservableSubjectEntity<T>() {
+
+    override val subject = PublishSubject<T>()
 
 }
